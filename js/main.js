faeffb76c0b48d86777c1b7507e1fe507cda22b4
padd0010 <padd0010@algonquinlive.com>
2014-10-07 10:16:23 -0400

    Waypoints & Exercise 4
    added waypoints plugin and completed scrolling effects exercise.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 var $win = $(window);
 var $sun = $('.sun');
 var $sunSection = $('.sun-section');
+var $dipper = $('.dipper');
+var $dipperSection = $('.dipper-section');
+var $shuttle = $('.shuttle');
+var $shuttleSection = $('.shuttle-section');
 
 $win.on('scroll', function () { 
 	var scrollPos = $win.scrollTop();
@@ -8,3 +12,11 @@ $win.on('scroll', function () {
 	$sun.css('transform', 'rotate(' + scrollPos / 5 + 'deg)');
 	$sunSection.css('background-position', 'center ' + scrollPos / 3 + 'px');
 });
+
+$dipperSection.waypoint(function () {
+	$dipper.addClass('js-dipper-fade');
+}, {offset: '50%' });
+
+$shuttleSection.waypoint(function () {
+	$shuttle.addClass('js-shuttle-slide');
+}, {offset: '50%' });
